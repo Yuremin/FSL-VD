da85302bdef0433cfc361af777a39d4c83a9ee92
yurem <yuzhmin@126.com>
2022-10-27 16:06:10 +0800

    update

---
+++ README.md
@@ -0,0 +1,28 @@
+Fine-tune CodeBERT for Multi-Class Classification of Vulnerability based on Few-Shot Learning
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+数据集
+
+（1）C语言：
+
++ NVD
++ SARD
+
+（2）Solidity语言
+
++ ESC等
---
+++ src/run.py
@@ -0,0 +1,12 @@
+import torch
+from codebert.unixcoder import UniXcoder
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model = UniXcoder("microsoft/unixcoder-base")
+model.to(device)
+
+
+func = "def f(a,b): if a>b: return a else return b"
+tokens_ids = model.tokenize([func],max_length=512,mode="<encoder-only>")
+source_ids = torch.tensor(tokens_ids).to(device)
+tokens_embeddings,max_func_embedding = model(source_ids)
